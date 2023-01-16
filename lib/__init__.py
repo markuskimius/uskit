26820d8bdbed6dc90e561178119a516a540ca4c6
Mark Kim <markuskimius@gmail.com>
2023-01-16 11:19:27 -0500

    fix version

--- lib/__init__.py
+++ lib/__init__.py
@@ -1,7 +1,1 @@
-__copyright__ = "Copyright 2022-2023 Mark Kim"
-__license__ = "Apache 2.0"
-__version__ = "0.0.1"
-__author__ = "Mark Kim"
-
 from .uskit import *
-
--- lib/uskit/__init__.py
+++ lib/uskit/__init__.py
@@ -4,3 +4,9 @@ from .abstract_session import *
 from .query_service import *
 from .txn_service import *
 from .server import *
+
+__copyright__ = "Copyright 2022-2023 Mark Kim"
+__license__ = "Apache 2.0"
+__version__ = "0.0.1"
+__author__ = "Mark Kim"
+
