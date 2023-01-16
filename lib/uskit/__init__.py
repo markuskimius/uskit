0b8a11cc3518940bcf6e1b9eebae1419b54de37a
Mark Kim <markuskimius@gmail.com>
2023-01-16 11:41:55 -0500

    package meta strings

--- lib/uskit/__init__.py
+++ lib/uskit/__init__.py
@@ -5,7 +5,7 @@ from .query_service import *
 from .txn_service import *
 from .server import *
 
-__copyright__ = "Copyright 2022-2023 Mark Kim"
+__copyright__ = "2022-2023 Mark Kim"
 __license__ = "Apache 2.0"
 __version__ = "0.0.1"
 __author__ = "Mark Kim"
--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@
 from setuptools import setup, find_packages
 from glob import glob
 from os import path
+import uskit
 
 SCRIPTDIR = path.abspath(path.dirname(__file__))
 
@@ -10,11 +11,11 @@ with open(path.join(SCRIPTDIR, "README.md"), encoding="utf-8") as f:
     long_description = f.read()
 
 setup(
-                             name = "uskit",
+                             name = uskit.__name__,
                       description = "A microservices kit",
-                          version = "0.0.1",
-                          license = "Apache 2.0",
-                           author = "Mark Kim",
+                          version = uskit.__version__,
+                          license = uskit.__license__,
+                           author = uskit.__author__,
                      author_email = "markuskimius+py@gmail.com",
                               url = "https://github.com/markuskimius/uskit",
                          keywords = [ "microservice", "framework" ],
