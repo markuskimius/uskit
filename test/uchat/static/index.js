8d5eae63b66b5255b978c7147d32ed924a814446
Mark Kim <markuskimius@gmail.com>
2023-02-20 10:23:11 -0500

    simplify client

--- test/uchat/static/index.js
+++ test/uchat/static/index.js
@@ -7,51 +7,51 @@ import * as debug from "/uskit/debug.js";
 */
 
 async function main() {
-    const session = await uskit.createSession();
-    const loginSession = new LoginSession(session);
+    const session = uskit.session();
+    const loginProxy = new LoginProxy(session);
 
     /* Clients */
-    const loginTxnClient = await uskit.createTxnClient(session, "LOGIN");
-    const addUserTxnClient = await uskit.createTxnClient(loginSession, "TXN_USER_ADD");
-    const editUserTxnClient = await uskit.createTxnClient(loginSession, "TXN_USER_EDIT");
-    const deleteUserTxnClient = await uskit.createTxnClient(loginSession, "TXN_USER_DELETE");
-    const chatTxnClient = await uskit.createTxnClient(loginSession, "TXN_CHAT");
-    const userQueryClient = await uskit.createQueryClient(loginSession, "QUERY_USER");
-    const chatQueryClient = await uskit.createQueryClient(loginSession, "QUERY_CHAT");
+    const loginTxn = uskit.txn_client(loginProxy, "LOGIN");
+    const addUserTxn = uskit.txn_client(loginProxy, "TXN_USER_ADD");
+    const editUserTxn = uskit.txn_client(loginProxy, "TXN_USER_EDIT");
+    const deleteUserTxn = uskit.txn_client(loginProxy, "TXN_USER_DELETE");
+    const chatTxn = uskit.txn_client(loginProxy, "TXN_CHAT");
+    const userQuery = uskit.query_client(loginProxy, "QUERY_USER");
+    const chatQuery = uskit.query_client(loginProxy, "QUERY_CHAT");
 
     /* Widgets */
-    const pageWidget = await uskit.createPageWidget("UChat");
-    const loginDialogWidget = await uskit.createDialogWidget("Sign In", { "contentUrl" : "./dialog-login.html", "cancel" : false });
-    const userTableWidget = await uskit.createTableWidget("User", { "controlUrl" : "./control-user.html", "sortBy" : "f_USER_ID" });
-    const chatTableWidget = await uskit.createTableWidget("Chat", { "controlUrl" : "./control-chat.html", "rowselect" : false, "usersortable" : false, "userfilterable" : false, "sortBy" : "f_TIMESTAMP", "sortDir" : "asc" });
-    const addUserDialogWidget = await uskit.createDialogWidget("Add User", { "contentUrl" : "./dialog-adduser.html" });
-    const editUserDialogWidget = await uskit.createDialogWidget("Edit User", { "contentUrl" : "./dialog-edituser.html" });
-    const deleteUserDialogWidget = await uskit.createDialogWidget("Delete User", { "contentUrl" : "./dialog-deleteuser.html" });
+    const page = await uskit.page_widget("UChat");
+    const loginDialog = await uskit.dialog_widget("Sign In", { "contentUrl" : "./dialog-login.html", "cancel" : false });
+    const userTable = await uskit.table_widget("User", { "controlUrl" : "./control-user.html", "sortBy" : "f_USER_ID" });
+    const chatTable = await uskit.table_widget("Chat", { "controlUrl" : "./control-chat.html", "rowselect" : false, "usersortable" : false, "userfilterable" : false, "sortBy" : "f_TIMESTAMP", "sortDir" : "asc" });
+    const addUserDialog = await uskit.dialog_widget("Add User", { "contentUrl" : "./dialog-adduser.html" });
+    const editUserDialog = await uskit.dialog_widget("Edit User", { "contentUrl" : "./dialog-edituser.html" });
+    const deleteUserDialog = await uskit.dialog_widget("Delete User", { "contentUrl" : "./dialog-deleteuser.html" });
 
     /* Page layout */
-    pageWidget.addChildWidget(loginDialogWidget);
-    pageWidget.addChildWidget(userTableWidget);
-    pageWidget.addChildWidget(chatTableWidget);
-    pageWidget.addChildWidget(addUserDialogWidget);
-    pageWidget.addChildWidget(editUserDialogWidget);
-    pageWidget.addChildWidget(deleteUserDialogWidget);
+    page.add("content", loginDialog);
+    page.add("content", userTable);
+    page.add("content", chatTable);
+    page.add("content", addUserDialog);
+    page.add("content", editUserDialog);
+    page.add("content", deleteUserDialog);
 
     /* Table layout */
-    userTableWidget.getElementOf("table").style.maxHeight = "30em";
-    chatTableWidget.getElementOf("table").style.maxHeight = "30em";
+    userTable.get("table").style.maxHeight = "30em";
+    chatTable.get("table").style.maxHeight = "30em";
 
     /* Connectors */
-    uskit.connectQueryToTable(userQueryClient, userTableWidget);
-    uskit.connectQueryToTable(chatQueryClient, chatTableWidget);
-    uskit.connectWidgetToTxn(loginDialogWidget, loginSession, "submit");
-    uskit.connectWidgetToTxn(loginSession, loginTxnClient, "submit");
-    uskit.connectWidgetToTxn(chatTableWidget, chatTxnClient, "submit");
-    uskit.connectWidgetToTxn(addUserDialogWidget, addUserTxnClient, "submit");
-    uskit.connectWidgetToTxn(editUserDialogWidget, editUserTxnClient, "submit");
-    uskit.connectWidgetToTxn(deleteUserDialogWidget, deleteUserTxnClient, "submit", () => {
+    uskit.x_query_table(userQuery, userTable);
+    uskit.x_query_table(chatQuery, chatTable);
+    uskit.x_widget_txn(loginDialog, loginProxy, "submit");
+    uskit.x_widget_txn(loginProxy, loginTxn, "submit");
+    uskit.x_widget_txn(chatTable, chatTxn, "submit");
+    uskit.x_widget_txn(addUserDialog, addUserTxn, "submit");
+    uskit.x_widget_txn(editUserDialog, editUserTxn, "submit");
+    uskit.x_widget_txn(deleteUserDialog, deleteUserTxn, "submit", () => {
         const data = [];
 
-        userTableWidget.getData("selectall").forEach((row) => {
+        userTable.data("selectall").forEach((row) => {
             data.push({
                 "USER_ID" : Number(row.f_USER_ID),
             });
@@ -61,19 +61,19 @@ async function main() {
     });
 
     /* Page events */
-    session.addEventObserver("open", async () => await pageWidget.setStatus("Connected."));
-    session.addEventObserver("close", async () => await pageWidget.setStatus("Disconnected.", "UCON"));
-    loginSession.addEventObserver("open", async () => await pageWidget.setStatus("Logged in."));
+    session.on("open", () => page.set("status", "Connected."));
+    session.on("close", () => page.set("status", "Disconnected."));
+    loginProxy.on("open", () => page.set("status", "Logged in."));
 
     /* Table events */
-    userTableWidget.addEventObserver("add", async () => await addUserDialogWidget.show());
-    userTableWidget.addEventObserver("edit", async () => await editUserDialogWidget.show());
-    userTableWidget.addEventObserver("delete", async () => await deleteUserDialogWidget.show());
+    userTable.on("add", () => addUserDialog.show());
+    userTable.on("edit", () => editUserDialog.show());
+    userTable.on("delete", () => deleteUserDialog.show());
 
     /* Txn events */
-    chatTxnClient.addEventObserver("ack", async () => {
-        const tableElement = chatTableWidget.getElementOf("table");
-        const inputElement = chatTableWidget.getElementOf("control").querySelector("input");
+    chatTxn.on("ack", () => {
+        const tableElement = chatTable.get("table");
+        const inputElement = chatTable.get("control").querySelector("input");
 
         tableElement.scrollTo(0, tableElement.scrollHeight);
 
@@ -81,97 +81,117 @@ async function main() {
     });
 
     /* Dialog setup */
-    addUserDialogWidget.addEventObserver("show", async () => addUserDialogWidget.clearData());
-    editUserDialogWidget.addEventObserver("show", async () => editUserDialogWidget.setData(userTableWidget.getData("selectfirst"), "table"));
-    deleteUserDialogWidget.addEventObserver("show", async () => {
-        const selections = userTableWidget.getData("selectall");
+    addUserDialog.on("show", () => addUserDialog.clear());
+    editUserDialog.on("show", () => editUserDialog.data("table", userTable.data("selectfirst")));
+    deleteUserDialog.on("show", () => {
+        const selections = userTable.data("selectall");
 
-        if(selections.length == 1) deleteUserDialogWidget.setContent(`Delete ${selections[0]["f_USER_NAME"]}?`);
-        else                       deleteUserDialogWidget.setContent(`Delete ${selections.length} users?`);
+        if(selections.length == 1) deleteUserDialog.setText("content", `Delete ${selections[0]["f_USER_NAME"]}?`);
+        else                       deleteUserDialog.setText("content", `Delete ${selections.length} users?`);
     });
 
     /* Widget visibility */
-    loginTxnClient.addEventObserver("ack",  async () => await loginDialogWidget.hide());
-    loginTxnClient.addEventObserver("nack", async () => await loginDialogWidget.show());
-    userQueryClient.addEventObserver("ack",  async () => await userTableWidget.show());
-    userQueryClient.addEventObserver("nack",  async () => await userTableWidget.hide());
-    chatQueryClient.addEventObserver("ack",  async () => await chatTableWidget.show());
-    chatQueryClient.addEventObserver("nack",  async () => await chatTableWidget.hide());
-    pageWidget.show();
+    loginTxn.on("ack",  () => loginDialog.hide());
+    loginTxn.on("nack", () => loginDialog.show());
+    userQuery.on("ack",  () => userTable.show());
+    userQuery.on("nack",  () => userTable.hide());
+    chatQuery.on("ack",  () => chatTable.show());
+    chatQuery.on("nack",  () => chatTable.hide());
+    userTable.hide();
+    chatTable.hide();
 
     /* Button enable/disable */
-    userTableWidget.addEventObserver("select", async (event) => userTableWidget.getData("selectall").length == 1 ? userTableWidget.enable("edit") : userTableWidget.disable("edit"));
-    userTableWidget.addEventObserver("select", async (event) => userTableWidget.getData("selectall").length >= 1 ? userTableWidget.enable("delete") : userTableWidget.disable("delete"));
-    userTableWidget.disable("edit");
-    userTableWidget.disable("delete");
+    userTable.on("select", (event) => userTable.data("selectall").length == 1 ? userTable.enable("edit") : userTable.disable("edit"));
+    userTable.on("select", (event) => userTable.data("selectall").length >= 1 ? userTable.enable("delete") : userTable.disable("delete"));
+    userTable.disable("edit");
+    userTable.disable("delete");
 
     /* Connect to the server */
-    await loginDialogWidget.show();
-    await session.open("/uchat");
+    loginDialog.show();
+    session.open("/uchat");
 }
 
 
 /**
-* LOGIN SESSION
+* LOGIN PROXY
 *
-* Login manager intercepts messages between the login dialog and login txn
+* Login proxy intercepts messages between the login dialog and login txn
 * then, if the session disconnects then reconnects, auto re-logs into the
 * server.
 */
-class LoginSession extends uskit.AbstractClient {
-    constructor(parentClient) {
-        super(parentClient);
+class LoginProxy {
+    #session = null;
+    #lastOpen = null;
+    #lastSubmit = null;
+    #eventManager = uskit.event_manager();
 
-        this.parentClient = parentClient;
-        this.last_open = null;
-        this.last_submit = null;
-
-        this.parentClient.addEventObserver("open", async (event) => await this.#onOpen(event));
+    constructor(session) {
+        this.#session = session;
+        this.#session.on("open", (event) => this.#on_open(event));
     }
 
-    addEventObserver(eventType, observer) {
-        switch(eventType) {
-            case "ack"    : /* pass through */
-            case "nack"   : /* pass through */
-            case "open"   : /* pass through */
-            case "submit" : super.addEventObserver(eventType, observer); break;
-            default       : this.parentClient.addEventObserver(eventType, observer);
+    on(type, handler) {
+        switch(type) {
+            case "ack"    : break;
+            case "nack"   : break;
+            case "open"   : break;
+            case "submit" : break;
+            default       : this.#session.on(type, (event) => this.trigger(event)); break;
+        }
+
+        this.#eventManager.on(type, handler);
+    }
+
+    trigger(event) {
+        switch(event.type) {
+            case "ack"    : this.#on_ack(event); break;
+            case "nack"   : this.#on_nack(event); break;
+            default       : this.#eventManager.trigger(event); break;
         }
     }
 
-    addMessageObserver(messageType, observer) {
-        this.parentClient.addMessageObserver(messageType, observer);
+    send(message) {
+        const sent = this.#session.send(message);
+
+        switch(message.MESSAGE_TYPE) {
+            case "LOGIN" : this.#lastSubmit = message; break;
+        }
+
+        return sent;
     }
 
-    async #onOpen(event) {
-        this.last_open = event;
+    isopen() {
+        this.#session.isopen();
+    }
+
+    close(message) {
+        this.#session.close();
+    }
+
+    #on_open(event) {
+        this.#lastOpen = event;
 
         /* Re-login */
-        if(this.last_submit) {
-            await super.notifyEventObservers(this.last_submit.type, this.last_submit);
+        if(this.#lastSubmit) {
+            this.send(this.#lastSubmit);
         }
     }
 
-    async onSubmit(event) {
-        this.last_submit = event;
-
-        await super.notifyEventObservers(event.type, event);
-    }
-
-    async onAck(event) {
-        await super.notifyEventObservers(event.type, event);
+    #on_ack(event) {
+        this.#eventManager.trigger(event);
 
         /* Login accepted; session is now open */
-        await super.notifyEventObservers("open", Object.assign({}, event, {
-            "type" : "open",
-        }));
+        this.#eventManager.trigger({
+            ...this.#lastOpen,
+            "session" : this,
+        });
     }
 
-    async onNack(event) {
-        /* Login rejected; credential is bad */
-        this.credentials = null;
+    #on_nack(event) {
+        /* Login rejected; submit was bad */
+        this.#lastSubmit = null;
 
-        await super.notifyEventObservers(event.type, event);
+        this.#eventManager.trigger(event);
     }
 }
 
@@ -180,5 +200,5 @@ class LoginSession extends uskit.AbstractClient {
 * BOOTSTRAP
 */
 
-main();
+await main();
 
